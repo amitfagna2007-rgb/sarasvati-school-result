2ec88471912e034fe1cb59b2190a8a23a7138c29
amitfagna2007-rgb <amitfagna2007@gmail.com>
2026-09-16 17:00:09 +0530

    Fix final PWA icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'sarasvati-school-v1.0.3';
-const APP_ASSETS = ['./','./index.html','./manifest.json','./version.json','./icon.svg','./icon-192.jpg?v=3'];
+const CACHE_NAME = 'sarasvati-school-v1.0.4';
+const APP_ASSETS = ['./','./index.html','./manifest.json','./version.json','./icon-192.png?v=4'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)).then(() => self.skipWaiting()));
 });
