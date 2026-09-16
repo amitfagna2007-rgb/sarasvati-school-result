155648f90e490848556309742e118129018425ca
amitfagna2007-rgb <amitfagna2007@gmail.com>
2026-09-16 17:06:24 +0530

    Align service worker cache version with app 1.0.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sarasvati-school-v1.0.4';
+const CACHE_NAME = 'sarasvati-school-v1.0.1';
 const APP_ASSETS = ['./','./index.html','./manifest.json','./version.json','./icon-192.png?v=4'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)).then(() => self.skipWaiting()));
