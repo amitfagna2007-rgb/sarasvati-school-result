dd8b73b5b447f75f1cb48d1db236274c4192dc3d
amitfagna2007-rgb <amitfagna2007@gmail.com>
2026-09-16 16:50:30 +0530

    Refresh PWA cache for new app icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'sarasvati-school-v1.0.2';
-const APP_ASSETS = ['./','./index.html','./manifest.json','./version.json','./icon.svg','./icon-192.png'];
+const CACHE_NAME = 'sarasvati-school-v1.0.3';
+const APP_ASSETS = ['./','./index.html','./manifest.json','./version.json','./icon.svg','./icon-192.jpg?v=3'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)).then(() => self.skipWaiting()));
 });
